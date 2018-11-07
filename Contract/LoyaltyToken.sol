42420f2e5723fc2d2e0af68df3d985f3348706d3
doga ozcan <dogasava@gmail.com>
2018-11-07 22:30:24 +0300

    Update LoyaltyToken.sol

--- Contract/LoyaltyToken.sol
+++ Contract/LoyaltyToken.sol
@@ -1,60 +1,154 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.18;
 
-contract LoyaltyToken
+ 
+
+contract ERC20Interface {
+
+    function totalSupply() public constant returns (uint);
+
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+
+    function transfer(address to, uint tokens) public returns (bool success);
+
+ 
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+
+}
+
+ 
+
+library SafeMath {
+
+    function add(uint a, uint b) internal pure returns (uint c) {
+
+        c = a + b;
+
+        require(c >= a);
+
+    }
+
+    function sub(uint a, uint b) internal pure returns (uint c) {
+
+        require(b <= a);
+
+        c = a - b;
+
+    }
+
+    function mul(uint a, uint b) internal pure returns (uint c) {
+
+        c = a * b;
+
+        require(a == 0 || c / a == b);
+
+    }
+
+    function div(uint a, uint b) internal pure returns (uint c) {
+
+        require(b > 0);
+
+        c = a / b;
+
+    }
+
+}
+
+ 
+
+contract LoyaltyToken is ERC20Interface
+
 {
-  //token supply added 
-  uint256 public constant totalSupply = 1000000;
+
+  using SafeMath for uint;
+
+  //token supply added
+
+  uint256 public constant _totalSupply = 1000000;
+
   mapping(address=>uint256) balances;
+
   string public constant symbol = "LYCT";
+
   string public constant name = "loyalty card token";
+
   uint8 public constant decimals = 3;
+
   address owner;
+
+ 
+
   
-  
+
   //ETHEREUM BASINA 500 LYCT VERİLECEK
+
   uint256 public constant rate = 500;
-  
+
+ 
+
   constructor() public {
-      balances[msg.sender]=totalSupply;
-  }
-  
-  function balanceOf(address _owner) public view returns(uint256 balance){
-      return balances[_owner]; 
+
+      balances[msg.sender]=_totalSupply;
+
   }
 
-  function contractBalance() public view returns(uint256 balance){
-      return balances[owner];
+ 
+
+  function balanceOf(address _owner) public view returns(uint256 balance){
+
+      return balances[_owner];
+
   }
-  
+
+ 
+
+  function transfer(address to, uint tokens) public returns (bool success) {
+
+        balances[msg.sender] = balances[msg.sender].sub(tokens);
+
+        balances[to] = balances[to].add(tokens);
+
+        emit Transfer(msg.sender, to, tokens);
+
+        return true;
+
+    }
+
+   
+
   //fallback function tanımlıyoruz. contract adresine ether yolladığında bu fonksiyon otomatik çalışır
-  
+
+ 
+
   function() public payable{
+
       donationPay();
+
   }
-  
+
+ 
+
   function donationPay() public payable {
+
           require(
+
           msg.value>=0
+
           );
-      
+
+     
+
       //ether gönderen kişinin nekadar token alacağı hesaplanıyor
-      uint256 userDonationValue = mul(msg.value,rate);
-      balances[msg.sender] =add(balances[msg.sender],userDonationValue);
+
+      uint256 userDonationValue = msg.value.mul(rate);
+
+      balances[msg.sender] =balances[msg.sender].add(userDonationValue);
+
       owner.transfer(msg.value);
   }
-
-  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    if (a == 0) {
-      return 0;
+   function transferAnyERC20Token(address tokenAddress, uint tokens) public  returns (bool success) {
+        return ERC20Interface(tokenAddress).transfer(owner, tokens);
     }
-    c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    c = a + b;
-    assert(c >= a);
-    return c;
-  }
 }
